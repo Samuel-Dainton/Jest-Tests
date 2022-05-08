3bac83da0d91d92d454cd1489e52f19a09f06c06
Samuel Dainton <dexterdainton@gmail.com>
2022-05-08 10:26:33 +0000

    Feat: functions for newGame, addTurn, showScore, lightsOn, showTurns and corresponding tests.

---
+++ scripts/game.js
@@ -0,0 +1,47 @@
+let game = {
+    score: 0,
+    currentGame: [],
+    playerMoves: [],
+    choices: ["button1", "button2", "button3", "button4"],
+    turnNumber: 0,
+}
+
+function newGame() {
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore();
+    addTurn();
+    game.turnNumber = 0;
+}
+
+function addTurn() {
+    game.playerMoves = []
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    showTurns()
+
+}
+
+function showScore() {
+    document.getElementById("score").innerText = game.score;
+}
+
+function lightsOn(circ) {
+    document.getElementById(circ).classList.add("light")
+    setTimeout(() => {
+        document.getElementById(circ).classList.remove("light");
+    }, 400)
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800)
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
